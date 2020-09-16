77986cd5f8f9d7c826bf7635e00a02a8aa79375f
vahagn@!nvision.am <nvisionMicrosoft12>
2020-09-16 17:32:16 +0400

    A terribly wrong push

--- include/ifeature_extractor.h
+++ include/ifeature_extractor.h
@@ -4,11 +4,21 @@
 
 #ifndef INC_3D_TELEMETRY_IFEATURE_EXTRACTOR_H
 #define INC_3D_TELEMETRY_IFEATURE_EXTRACTOR_H
+#include <vector>
+#include <Eigen/Dense>
 
-namespace gago{
-class IFeatureExtractor{
+namespace gago {
+
+template<typename TLandmarkDescriptor, typename TCoord>
+class IFeatureExtractor {
  public:
+  virtual void Extract(uint8_t *rgb,
+                       int w,
+                       int h,
+                       Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> & out_descriptors,
+                       Eigen::Matrix<TCoord, Eigen::Dynamic, 2> & out_keypoints) = 0;
 };
+
 }
 
 #endif //INC_3D_TELEMETRY_IFEATURE_EXTRACTOR_H
